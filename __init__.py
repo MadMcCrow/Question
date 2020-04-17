d332da67cb05fe24cfac13b1ca1d5e19f13ba717
MadMcCrow <https://MadMcCrow@github.com>
2020-04-17 20:24:59 +0200

    fixing __init__ for import in other project as git submodule

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-#import all necessary modules
-import .Question_Prompt    
-import .Question_Menu
-import .Question_Action
+# make sure this folder is in your python path so that you can import it really easily :
+import sys
+from os import path
+realpath = path.dirname(path.abspath(__file__))
+# we use sys.path.insert(1, realpath) to avoid some errors with paths
+# sys.path.append(realpath)
+sys.path.insert(1, realpath)
